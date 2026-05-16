885ee40c5161889048637a7d4ebd40107573a046
milos67 <milosneskovic67@gmail.com>
2026-05-16 13:35:51 +0200

    Update env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,5 @@
 export const env = {
   apiBaseUrl: import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:8674/api',
   travelApiBaseUrl: import.meta.env.VITE_TRAVEL_API_BASE_URL ?? 'http://localhost:5056/api',
-  sharingApiBaseUrl: import.meta.env.VITE_SHARING_API_BASE_URL ?? 'http://localhost:5124/api',
+  sharingApiBaseUrl: import.meta.env.VITE_SHARING_API_BASE_URL ?? 'http://localhost:8636/api',
 }
